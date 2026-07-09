3e126484aa62abcb03d6e178a9cf863ffb5cbb62
ADHI prasetyo wibowo <adhiprasetyow07@gmail.com>
2026-07-09 07:13:30 +0700

    UI: Prevent auto-zoom on mobile devices when focusing inputs

--- backend/resources/views/app.blade.php
+++ backend/resources/views/app.blade.php
@@ -2,7 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="notranslate">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
         
         @if(isset($seo))
             <title>{{ $seo['title'] }} - InternHub</title>
